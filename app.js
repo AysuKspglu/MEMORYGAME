675584e3eb501b4d65572882ec4f57d96facbb31
AysuKspglu <aysukasapogluu@gmail.com>
2025-10-05 23:19:38 +0300

    first step
    first step ok

---
+++ app.js
@@ -0,0 +1,56 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: "images/fries.jpg"
+    },
+    {
+        name: 'hamburger',
+        img: 'images/hamburger.jpg'
+
+    },
+    {
+        name: "cheesecake",
+        img: "images/cheesecake.jpg"
+    },
+    {
+        name: "lipstick",
+        img: "images/lipstick.jpg"
+    },
+    {
+        name: "pizza",
+        img: "images/pizza.jpg"
+    },
+    {
+        name: "shoes",
+        img: "images/shoes.jpg"
+    },
+    {
+        name: "fries",
+        img: "images/fries.jpg"
+    },
+    {
+        name: "hamburger",
+        img: "images/hamburger.jpg"
+    },
+    {
+        name: "cheesecake",
+        img: "images/cheesecake.jpg"
+    },
+    {
+        name: "lipstick",
+        img: "images/lipstick.jpg"
+    },
+    {
+        name: "pizza",
+        img: "images/pizza.jpg"
+    },
+    {
+        name: "shoes",
+        img: "images/shoes.jpg"
+
+    },
+]
+
+cardArray.sort(() => 0.5 - Math.random());
+
+console.log(cardArray);
